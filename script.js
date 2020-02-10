5f0cf1ebfa506dcb5d9be7da61e0662eec5a476a
Zack Friendly <friendly.wisconsin@gmail.com>
2020-02-10 13:14:06 -0500

    built phase 1 game functionality, minus reset button

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-let gameSquares = document.querySelectorAll(".game__squares");
-console.log(gameSquares);
-gameSquares.addEventListener("click", turnFunction);
-function turnFunction(e) {
-  e.preventDefault();
-  console.log("Clicked!");
+let squares = document.querySelectorAll(".game__squares");
+let gameTurn = 0;
+let turnCounter = document.querySelector(".game__turn_counter");
+let gameReset = document.querySelector(".game__reset");
+
+for (i = 0; i < squares.length; i++) {
+  squares[i].setAttribute("id", i + 1);
+  let color = squares[i].color;
+  function turnFunction() {
+    gameTurn += 1;
+    if (color === "red" || color === "blue") {
+      gameTurn -= 1;
+      alert("That square has been picked, pick another!");
+    } else if (gameTurn % 2 === 0 && gameTurn < 9) {
+      this.classList.add("red");
+      color = "red";
+      turnCounter.innerHTML = "Blue's Turn";
+    } else if (color !== "red" && gameTurn < 9) {
+      this.classList.add("blue");
+      color = "blue";
+      turnCounter.innerHTML = "Red's Turn";
+    }
+    if (gameTurn >= 9) {
+      this.classList.add("blue");
+      turnCounter.innerHTML = "GAME OVER!";
+    }
+  }
+  squares[i].addEventListener("click", turnFunction);
 }
+
+// gameReset.addEventListener("click", resetGame);
+//   function resetGame(e) {
+//     e.preventDefault();
+//     gameTurn = 0;
+//     this.target.classList.remove("blue");
+//     this.target.classList.remove("red");
+//   }
 //Step 1: Check to see who's turn it is (build that function)
 //Step 2: On a click in the square, turn it red or blue depending on who's turn it is
 //Step 3: Have the turns switch after every click
