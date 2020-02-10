e3b8a30e3251fc4ab05ce7326952478be728cf6d
Zack Friendly <friendly.wisconsin@gmail.com>
2020-02-10 16:37:14 -0500

    Finished all of the game except for draw function

--- script.js
+++ script.js
@@ -3,25 +3,28 @@ let gameTurn = 0;
 let turnCounter = document.querySelector(".game__turn_counter");
 let gameReset = document.querySelector(".game__reset");
 let winningNumbers = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7]
+  ["1", "2", "3"],
+  ["4", "5", "6"],
+  ["7", "8", "9"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["3", "6", "9"],
+  ["1", "5", "9"],
+  ["3", "5", "7"]
 ];
 let blue = [];
 let red = [];
 
-winningNumbers.forEach(winner => {
-  if (winner === blue) {
-    alert("Blue Wins!");
-  } else if (winner === red) {
-    alert("Red Wins");
-  }
-});
+//   if (blue.length === 3 || red.length === 3) {
+//     winningNumbers.forEach(winner => {
+//       if (winner.includes(...blue.sort())) {
+//         alert("Blue Wins!");
+//       } else if (winner.includes(...red.sort())) {
+//         alert("Red Wins");
+//       }
+//     });
+//   }
+// }
 
 for (let i = 0; i < squares.length; i++) {
   squares[i].setAttribute("id", i + 1);
@@ -33,13 +36,18 @@ function turnFunction(evt) {
   if (gameTurn % 2 === 0) {
     evt.target.classList.add("red");
     evt.target.innerHTML = "X";
-    red.push(parseInt(evt.target.id));
+    colorName = "red";
+    red.push(evt.target.id);
     turnCounter.innerHTML = "Blue's Turn";
+    checkForWinner(red);
   } else if (gameTurn % 2 !== 0) {
     evt.target.classList.add("blue");
     evt.target.innerHTML = "O";
-    blue.push(parseInt(evt.target.id));
+    colorName = "blue";
+    blue.push(evt.target.id);
     turnCounter.innerHTML = "Red's Turn";
+    checkForWinner(blue);
+    console.log("Check");
   }
   if (gameTurn >= 9) {
     evt.target.classList.add("blue");
@@ -51,9 +59,33 @@ function turnFunction(evt) {
 function resetGame(e) {
   e.preventDefault();
   gameTurn = 0;
+  red = [];
+  blue = [];
+  turnCounter.style["font-size"] = "30px";
+  turnCounter.innerHTML = "Blue's turn";
+  turnCounter.style.background = "white";
   squares.forEach(square => square.classList.remove("blue", "red"));
   squares.forEach(square => (square.innerHTML = ""));
   squares.forEach(square => square.addEventListener("click", turnFunction));
+}
+function checkForWinner(color) {
+  for (let i = 0; i < winningNumbers.length; i++) {
+    let count = 0;
+    for (let j = 0; j < color.length; j++) {
+      console.log(color, "color");
+      console.log(winningNumbers[i][j], "winningNumbers");
+      if (color.includes(winningNumbers[i][j]) === true) {
+        count = count + 1;
+        if (count === 3) {
+          turnCounter.style["font-size"] = "100px";
+          turnCounter.style.background = "gold";
+          turnCounter.innerHTML = `${colorName} Wins!`;
+          //   let rendered = document.querySelector(".rendered");
+          //   rendered.classList.add("active");
+        }
+      }
+    }
+  }
 }
 
 //Step 1: Check to see who's turn it is (build that function)
